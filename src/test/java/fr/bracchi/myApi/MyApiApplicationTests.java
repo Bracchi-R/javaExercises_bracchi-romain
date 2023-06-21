526e77e7d27b34b924fdc524b4fc166c34c5597e
Bracchi-R <bracchipro@gmail.com>
2023-06-21 13:42:31 +0200

    Spring Boot project initialized

---
+++ src/test/java/fr/bracchi/myApi/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bracchi.myApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
